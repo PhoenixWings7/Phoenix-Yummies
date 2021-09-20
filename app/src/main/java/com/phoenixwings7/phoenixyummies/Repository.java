86aea427d34bf9fa4e0c74ddacd0d97a56e2375f
PhoenixWings7 <adamus.klaudia.pasp@gmail.com>
2021-09-20 17:41:54 +0200

    create Repository class

---
+++ app/src/main/java/com/phoenixwings7/phoenixyummies/Repository.java
@@ -0,0 +1,10 @@
+package com.phoenixwings7.phoenixyummies;
+
+/**
+ * Repository is an entry point for all elements requesting data.
+ * It handles saving data to the local database,
+ * requesting data from the API and updating its listeners.
+ */
+public class Repository {
+    
+}
